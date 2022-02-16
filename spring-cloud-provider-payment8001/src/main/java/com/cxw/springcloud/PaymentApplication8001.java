e8e41c9f6d46c52333b542e655bfc5a1f47b553c
xuwen_chen <1054735193@qq.com>
2022-02-17 00:55:55 +0800

    step(支付模块)：provider服务搭建

---
+++ spring-cloud-provider-payment8001/src/main/java/com/cxw/springcloud/PaymentApplication8001.java
@@ -0,0 +1,18 @@
+package com.cxw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName PaymentApplication8001
+ * @Author xuwen_chen
+ * @Date 2022-02-15 23:45
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class PaymentApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8001.class, args);
+    }
+}
