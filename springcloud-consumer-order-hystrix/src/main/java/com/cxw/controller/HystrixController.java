6bdbde755e30d4f60c110aea1f25b41c4aa07ab8
xuwen_chen <1054735193@qq.com>
2022-02-22 01:32:56 +0800

    step(hystrix)：配置hystrix熔断

--- springcloud-consumer-order-hystrix/src/main/java/com/cxw/controller/HystrixController.java
+++ springcloud-consumer-order-hystrix/src/main/java/com/cxw/controller/HystrixController.java
@@ -3,6 +3,8 @@ package com.cxw.controller;
 import com.cxw.service.FeignHystrixServiceClient;
 import com.cxw.springcloud.entity.CommonResult;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -44,5 +46,24 @@ public class HystrixController {
 
     public CommonResult<String> globalFallback() {
         return new CommonResult<String>(500, "8000 hystrix global fallback", null);
+    }
+
+    @GetMapping("cir/{id}")
+    @HystrixCommand(fallbackMethod = "circuitFallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),//打开开关
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//熔断10s后进入半打开状态
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//请求线程数
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")//失败率达到多少后熔断
+    })
+    public String circuit(@PathVariable("id") Long id) {
+        //HystrixCommandProperties
+        if(id < 0) {
+            throw new RuntimeException();
+        }
+        return "success";
+    }
+
+    public String circuitFallback(@PathVariable("id") Long id){
+        return "熔断保护";
     }
 }
